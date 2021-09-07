a7202c099d04291b251b9756555d35c66eaa3980
anthony18611996 <52492865+anthony18611996@users.noreply.github.com>
2021-09-07 14:11:06 +0300

    Revert "Main commit"

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
 <script>
 import "./assets/js/scroll.js";
 import Layout from "./layouts/Layout";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default {
   name: "App",
@@ -12,6 +14,55 @@ export default {
     Layout,
   },
 };
+
+setTimeout(() => {
+  [].forEach.call(
+    document.querySelectorAll("input[type=tel]"),
+    function (input) {
+      let keyCode;
+      function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        let pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        let matrix = "+7 (___) ___-__-__",
+          i = 0,
+          def = matrix.replace(/\D/g, ""),
+          val = this.value.replace(/\D/g, ""),
+          newValue = matrix.replace(/[_\d]/g, function (a) {
+            return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+          });
+        i = newValue.indexOf("_");
+        if (i != -1) {
+          i < 5 && (i = 3);
+          newValue = newValue.slice(0, i);
+        }
+        let reg = matrix
+          .substr(0, this.value.length)
+          .replace(/_+/g, function (a) {
+            return "\\d{1," + a.length + "}";
+          })
+          .replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (
+          !reg.test(this.value) ||
+          this.value.length < 5 ||
+          (keyCode > 47 && keyCode < 58)
+        )
+          this.value = newValue;
+        if (event.type == "blur" && this.value.length < 5) this.value = "";
+      }
+
+      input.addEventListener("input", mask, false);
+      input.addEventListener("focus", mask, false);
+      input.addEventListener("blur", mask, false);
+      input.addEventListener("keydown", mask, false);
+    }
+  );
+}, 2000);
+
+AOS.init({
+  once: true,
+});
 </script>
 
 <style lang="scss">
@@ -53,5 +104,16 @@ div.wrapper {
 .slide-fade-leave-to {
   transform: translateX(-10px);
   opacity: 0;
+}
+
+@include media_mobile {
+  [data-aos][data-aos][data-aos-duration="2000"],
+  body[data-aos-duration="2000"] [data-aos] {
+    transition-duration: 1s;
+  }
+  [data-aos][data-aos][data-aos-duration="1500"],
+  body[data-aos-duration="1500"] [data-aos] {
+    transition-duration: 0.5s;
+  }
 }
 </style>
